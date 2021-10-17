91fabca910861736dcbe2e125f316d2302e187e1
Leanhogfutures <61065941+Leanhogfutures@users.noreply.github.com>
2021-10-16 22:48:52 -0800

    Update scripts.js

--- resources/scripts.js
+++ resources/scripts.js
@@ -3,3 +3,5 @@
 WolframNotebookEmbedder.embed('https://www.wolframcloud.com/obj/darthanchovy0/Published/ForWebsite.nb',document.getElementById('notebookContainer'));
 
 WolframNotebookEmbedder.embed('https://www.wolframcloud.com/obj/darthanchovy0/Published/ElectricFIeldPotential.nb',document.getElementById('notebookContainer'));
+
+WolframNotebookEmbedder.embed('https://www.wolframcloud.com/obj/darthanchovy0/Published/pendulum.nb',document.getElementById('notebookContainer'));
