b437426ae5d6ee1696143ce7d1c2fb6556c356c4
AnnQW <aqwade@gmail.com>
2023-04-03 14:51:51 -0700

    Update scripts.js

--- resources/scripts.js
+++ resources/scripts.js
@@ -8,3 +8,26 @@ WolframNotebookEmbedder.embed('https://www.wolframcloud.com/obj/tg021/Published/
 
 WolframNotebookEmbedder.embed(''https://www.wolframcloud.com/obj/tg021/Published/CubeRender.nb',document.getElementById('notebookContainer'));
 
+
+$(document).ready(function() {
+    $('.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    }, function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
+  });
+
+  
+  // get the navbar element to add text when the user scrolls down
+const navbar = document.getElementById('navbar');
+
+// listen for the scroll event on the window
+window.addEventListener('scroll', function() {
+  // if the user has scrolled down past the height of the navbar, add the "scroll" class to the navbar
+  if (window.scrollY > navbar.offsetHeight) {
+    navbar.classList.add('scroll');
+  } else {
+    // otherwise, remove the "scroll" class
+    navbar.classList.remove('scroll');
+  }
+});
